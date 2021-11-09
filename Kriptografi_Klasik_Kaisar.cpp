acb8e27cdbdd8f427312d4ddbbd9896e443e56ed
Mahendra Chevro <67460437+mchevro@users.noreply.github.com>
2021-11-09 18:17:45 +0700

    Create Kriptografi_Klasik_Kaisar.cpp

---
+++ Kriptografi_Klasik_Kaisar.cpp
@@ -0,0 +1,63 @@
+//ALGORITMA KODE KAISAR
+//REFRENCE : BUKU "PENGANTAR ILMU KRIPTOGRAFI" BY AMIKOM HAL 51
+//AUTHOR : MCHEVRO
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Global Deklarasi
+string plain_teks, cipher_teks;
+char e;
+int kunci;
+short option;
+
+//Encryption
+void encrypt() {
+    for (int i = 0; i < plain_teks.length(); i++) {
+        e = plain_teks[i];
+        cipher_teks = e + kunci % 26;
+        cout << cipher_teks;
+    }
+}
+
+
+//Decryption
+void decrypt() {
+    for (int i = 0; i < cipher_teks.length(); i++) {
+        e = cipher_teks[i];
+        plain_teks = e - kunci % 26;
+        cout << plain_teks;
+    }
+}
+
+int main() {
+    cout << "[1] Encrypt" << endl;
+    cout << "[2] Decrypt" << endl;
+    cout << "Pilih Opsi : ";
+    cin >> option;
+    cout << endl;
+
+    switch (option) {
+    case 1:
+        cout << "Teks Anda : ";
+        cin >> plain_teks;
+        cout << "Kunci : ";
+        cin >> kunci;
+        encrypt();
+        break;
+    case 2:
+        cout << "Encrypt Anda : ";
+        cin >> cipher_teks;
+        cout << "Kunci : ";
+        cin >> kunci;
+        decrypt();
+        break;
+    default:
+        cout << "Tidak Ada Pilihan!";
+        break;
+    }
+    cout << endl << endl;
+
+    return 0;
+}
