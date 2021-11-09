a6a1179db4bf87500454655e6cfaa41a96a9b421
Mahendra Chevro <67460437+mchevro@users.noreply.github.com>
2021-11-09 22:12:47 +0700

    Create Kriptografi_Klasik_Kaisar_ROT13.cpp

---
+++ Kriptografi_Klasik_Kaisar_ROT13.cpp
@@ -0,0 +1,61 @@
+//ALGORITMA KODE KAISAR
+//REFRENCE : BUKU "PENGANTAR ILMU KRIPTOGRAFI" BY AMIKOM HAL 51
+//AUTHOR : MCHEVRO
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Global Deklarasi
+string plain_teks, cipher_teks;
+char e;
+short option;
+
+//Encryption
+void encrypt() {
+    for (int i = 0; i < plain_teks.length(); i++) {
+        e = plain_teks[i];
+        cipher_teks = e + 13;
+        cout << cipher_teks;
+    }
+}
+
+
+//Decryption
+void decrypt() {
+    for (int i = 0; i < cipher_teks.length(); i++) {
+        e = cipher_teks[i];
+        plain_teks = e - 13;
+        cout << plain_teks;
+    }
+}
+
+int main() {
+    cout << "KODE KAISAR ROT13" << endl;
+    cout << "[1] Encrypt" << endl;
+    cout << "[2] Decrypt" << endl;
+    cout << "Pilih Opsi : ";
+    cin >> option;
+    cout << endl;
+
+    switch (option) {
+    case 1:
+        cout << "Teks Anda : ";
+        cin.ignore(); 
+        getline(cin, plain_teks);
+        encrypt();
+        break;
+    case 2:
+        cout << "Encrypt Anda : ";
+        cin.ignore(); 
+        getline(cin, cipher_teks);
+        decrypt();
+        break;
+    default:
+        cout << "Tidak Ada Pilihan!";
+        break;
+    }
+    cout << endl << endl;
+
+    return 0;
+}
