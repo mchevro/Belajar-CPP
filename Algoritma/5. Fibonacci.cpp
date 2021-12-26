d1a9be868ba80e21690abaa8b8853d0873fb50be
Mahendra Chevro <67460437+mchevro@users.noreply.github.com>
2021-12-26 19:10:00 +0700

    Update 5. Fibonacci.cpp

--- Algoritma/5. Fibonacci.cpp
+++ Algoritma/5. Fibonacci.cpp
@@ -8,17 +8,17 @@ int main(){
     
     for(int i=0; i<11; i++){
         if(i==0){
-            cout << "f" << i << ": " << f0 << endl;
+            cout << "f" << i << ": " << f0 << "+" << f1 << " = " <<  f0 << endl;
             continue;
         }else if(i==1){
-            cout << "f" << i << ": " << f1 << endl;
+            cout << "f" << i << ": " << f0 << "+" << f1 << " = " <<  f1 << endl;
             continue;
         }
         nextStep = f0 + f1; // 1, 2, 3, 5, 8, 13, 21, 34, 55
         f0 = f1; // 1, 1, 2, 3, 5, 8, 13, 21, 34              
         f1 = nextStep; // 1, 2, 3, 5, 8, 13, 21, 34, 55
         
-        cout << "f" << i << ": " << nextStep << endl;
+        cout << "f" << i << ": " << f0 << "+" << f1 << " = " <<  nextStep << endl;
     }
     return 0;
 }
